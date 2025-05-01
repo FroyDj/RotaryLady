22ae6ab43642601a8d95f14ae7c61b181a18a006
DC0REG <44757571+DC0REG@users.noreply.github.com>
2025-05-01 19:51:55 +0200

    Update speedup.pyx

--- speedup.pyx
+++ speedup.pyx
@@ -15,23 +15,23 @@ import param
 
 def pixelcount(int ppv_count, int triangle_count, int fancount):
 	cdef unsigned char[::1]			triangle_img_deck_ravel_view	= param.triangle_img_deck_ravel
-	cdef unsigned char[::1]			circle_mask						= param.circle_mask
-	cdef short unsigned int[::1]	img_sum_ravel_view				= param.img_sum_ravel
-	cdef Py_ssize_t					i, j
-	cdef unsigned int				pixel_total						= param.IMG_WIDTH * param.IMG_HEIGHT
-	cdef unsigned short				w = param.IMG_WIDTH
-	cdef unsigned int				start, length, mi, o
-	cdef int						ppv_count_last, r
-	cdef unsigned char				h
+	cdef unsigned char[::1]			circle_mask			= param.circle_mask
+	cdef short unsigned int[::1]		img_sum_ravel_view		= param.img_sum_ravel
+	cdef Py_ssize_t				i, j
+	cdef unsigned int	pixel_total = param.IMG_WIDTH * param.IMG_HEIGHT
+	cdef unsigned short	w = param.IMG_WIDTH
+	cdef unsigned int	start, length, mi, o
+	cdef int		ppv_count_last, r
+	cdef unsigned char	h
 		
 	start = 0
 	r = param.RESOLUTION
 	o = fancount * pixel_total
 	start = 0
 	length = pixel_total
-	for i in range(r): 					#für jedes dreieck im fan
-		mi = 0											# zaelt pixel im img
-		for j in range(start, length):		#für jeden pixel im img
+	for i in range(r): 
+		mi = 0
+		for j in range(start, length):
 			if triangle_img_deck_ravel_view[j] != 0:
 				h = triangle_img_deck_ravel_view[j]
 				triangle_img_deck_ravel_view[j] = 0
@@ -47,29 +47,29 @@ def pixelcount(int ppv_count, int triangle_count, int fancount):
 
 def init_calc(int ppv_count, int triangle_count, int fan_index, float xlaser, float ylaser):
 	cdef unsigned char[::1]			triangle_img_deck_ravel_view	= param.triangle_img_deck_ravel
-	cdef unsigned char[::1]			circle_mask						= param.circle_mask
-	cdef unsigned int[::1]			pixel_positions_view			= param.pixel_positions
-	cdef float[::1]					pixel_weights_view				= param.pixel_weights
-	cdef short unsigned int[::1]	img_sum_ravel_view				= param.img_sum_ravel
-	cdef int[::1]					fan_triangles_ravel_view		= param.fan_triangles_ravel
+	cdef unsigned char[::1]			circle_mask			= param.circle_mask
+	cdef unsigned int[::1]			pixel_positions_view		= param.pixel_positions
+	cdef float[::1]				pixel_weights_view		= param.pixel_weights
+	cdef short unsigned int[::1]		img_sum_ravel_view		= param.img_sum_ravel
+	cdef int[::1]				fan_triangles_ravel_view	= param.fan_triangles_ravel
 	cdef unsigned int[::1]			triagle_start_positions_view	= param.triagle_start_positions
-	cdef unsigned int[::1]			triangle_pixel_counts_view		= param.triangle_pixel_counts
-	cdef Py_ssize_t					i, j
-	cdef unsigned int				pixel_total						= param.IMG_WIDTH * param.IMG_HEIGHT
-	cdef unsigned short				w = param.IMG_WIDTH
-	cdef unsigned int				start, length, mi, hh
-	cdef float						distance, x, y
-	cdef int						ppv_count_last
-	cdef unsigned char				h
+	cdef unsigned int[::1]			triangle_pixel_counts_view	= param.triangle_pixel_counts
+	cdef Py_ssize_t				i, j
+	cdef unsigned int			pixel_total			= param.IMG_WIDTH * param.IMG_HEIGHT
+	cdef unsigned short			w = param.IMG_WIDTH
+	cdef unsigned int			start, length, mi, hh
+	cdef float				distance, x, y
+	cdef int				ppv_count_last
+	cdef unsigned char			h
 	
 	hh = fan_index * pixel_total
 	start = 0
 	r = param.RESOLUTION
 	ppv_count_last = ppv_count
-	for i in range(r): 					#für jedes dreieck im fan
-		mi = 0											# zaelt pixel im img
+	for i in range(r):
+		mi = 0
 		triagle_start_positions_view[triangle_count] = ppv_count
-		for j in range(start, start + pixel_total):		#für jeden pixel im img
+		for j in range(start, start + pixel_total):
 			if triangle_img_deck_ravel_view[j] != 0:
 				h = triangle_img_deck_ravel_view[j]
 				triangle_img_deck_ravel_view[j] = 0
@@ -89,10 +89,10 @@ def init_calc(int ppv_count, int triangle_count, int fan_index, float xlaser, fl
 	return ppv_count, triangle_count
 
 def match_opti(double th, int c):
-	cdef unsigned char[::1]				img_to_view
-	cdef unsigned char[::1]				img_to_view_r			= param.img_to_r
-	cdef unsigned char[::1]				img_to_view_g			= param.img_to_g
-	cdef unsigned char[::1]				img_to_view_b			= param.img_to_b
+	cdef unsigned char[::1]			img_to_view
+	cdef unsigned char[::1]			img_to_view_r			= param.img_to_r
+	cdef unsigned char[::1]			img_to_view_g			= param.img_to_g
+	cdef unsigned char[::1]			img_to_view_b			= param.img_to_b
 	
 	cdef float[::1]				img_is_view
 	cdef float[::1]				img_is_view_r			= param.img_is_r
@@ -104,14 +104,14 @@ def match_opti(double th, int c):
 	cdef float[::1]				multiplyer_view_g		= param.multiplier_g
 	cdef float[::1]				multiplyer_view_b		= param.multiplier_b
 	
-	cdef unsigned int[::1]		pixel_positions_view	= param.pixel_positions
+	cdef unsigned int[::1]			pixel_positions_view		= param.pixel_positions
 	cdef float[::1]				pixel_weights_view		= param.pixel_weights
-	cdef unsigned int[::1]		triagle_start_positions_view	= param.triagle_start_positions
-	cdef unsigned int[::1]		triangle_pixel_counts_view		= param.triangle_pixel_counts
+	cdef unsigned int[::1]			triagle_start_positions_view	= param.triagle_start_positions
+	cdef unsigned int[::1]			triangle_pixel_counts_view	= param.triangle_pixel_counts
 	
-	cdef unsigned int	start, length, pix_pos
+	cdef unsigned int		start, length, pix_pos
 	cdef double			current_dist, current_dist_last, pot_fut_dist, diff, improv
-	cdef Py_ssize_t		i, j, count = 0
+	cdef Py_ssize_t			i, j, count = 0
 	cdef float			as_is, to_be, multiplyer, pwv
 	cdef str 			col_str 
 	
